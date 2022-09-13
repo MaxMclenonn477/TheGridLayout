3b68ea30f2fba0298a9e50d9331d08204f4902bc
MaxMclenonn477 <90787348+MaxMclenonn477@users.noreply.github.com>
2022-09-13 12:53:45 -0500

    made second page with new grid

---
+++ app/src/main/java/com/example/thegridlayout/AnotherGrid.java
@@ -0,0 +1,14 @@
+package com.example.thegridlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnotherGrid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another_grid);
+    }
+}
